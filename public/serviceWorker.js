575635923e08b1242511b4127d2c28e98fe8d87b
Keketso Lithane <keketso.lithane@bothouniversity.com>
2025-09-24 15:00:03 +0200

    Add missing StatusBadge and mockDb modules

---
+++ public/serviceWorker.js
@@ -0,0 +1,29 @@
+// serviceWorker.js
+export function register() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
+      
+      navigator.serviceWorker
+        .register(swUrl)
+        .then((registration) => {
+          console.log('SW registered: ', registration);
+        })
+        .catch((registrationError) => {
+          console.log('SW registration failed: ', registrationError);
+        });
+    });
+  }
+}
+
+export function unregister() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready
+      .then((registration) => {
+        registration.unregister();
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  }
+}
