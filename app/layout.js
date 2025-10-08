347b5e9661de5f47a629b06da85a0d53263fe18d
Keketso Lithane <keketso.lithane@bothouniversity.com>
2025-10-08 16:05:28 +0200

    errors

--- app/layout.js
+++ app/layout.js
@@ -7,17 +7,20 @@ import "./globals.css";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import ErrorBoundary from "../components/ErrorBoundary";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="min-h-screen flex flex-col bg-gray-50">
-        <Header />
-        <NavBar />
-        <main className="flex-1 container mx-auto py-8 px-4">
-          {children}
-        </main>
-        <Footer />
+        <ErrorBoundary>
+          <Header />
+          <NavBar />
+          <main className="flex-1 container mx-auto py-8 px-4">
+            {children}
+          </main>
+          <Footer />
+        </ErrorBoundary>
       </body>
     </html>
   );
